065accf8323d896d7a4de6e097a8efbe1fd8f63d
aldairdiaz <irving.s21diaz@gmail.com>
2023-03-04 15:38:33 -0600

    "Complete frontend"

--- app/src/main/java/com/adiaz/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/adiaz/calculatorapp/MainActivity.kt
@@ -3,15 +3,26 @@ package com.adiaz.calculatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var screenInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        screenInput = findViewById(R.id.screen)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
+        screenInput?.append((view as Button).text)
     }
+
+    fun onClear(view: View) {
+        screenInput?.text = ""
+    }
+
 }
